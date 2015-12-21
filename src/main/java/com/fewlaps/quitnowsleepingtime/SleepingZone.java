18a39b7e7838bdb7eafb2f3140bc848744356712
Roc Boronat <roc@fewlaps.com>
2015-12-21 21:01:43 +0100

    Meeting the OCP at SleepingZone

--- src/main/java/com/fewlaps/quitnowsleepingtime/SleepingZone.java
+++ src/main/java/com/fewlaps/quitnowsleepingtime/SleepingZone.java
@@ -1,25 +1,48 @@
 package com.fewlaps.quitnowsleepingtime;
 
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class SleepingZone {
     Locale locale;
+
+    private static final Map<String, SleepTimes> times = new HashMap<String, SleepTimes>();
+
+    public static final String DEFAULT_COUNTRY_CODE = "us";
+
+    static {
+        times.put(DEFAULT_COUNTRY_CODE, new SleepTimes(new HourMinute(23, 54), new HourMinute(7, 20)));
+        times.put("es", new SleepTimes(new HourMinute(0, 32), new HourMinute(8, 5)));
+    }
 
     public SleepingZone(Locale locale) {
         this.locale = locale;
     }
 
     public HourMinute getBedtime() {
-        if (locale.getCountry().equalsIgnoreCase("es")) {
-            return new HourMinute(0, 32);
+        SleepTimes st = times.get(locale.getCountry().toLowerCase());
+        if (st != null) {
+            return st.bedTime;
         }
-        return new HourMinute(23, 54);
+        return times.get(DEFAULT_COUNTRY_CODE).bedTime;
     }
 
     public HourMinute getWakeUp() {
-        if (locale.getCountry().equalsIgnoreCase("es")) {
-            return new HourMinute(8, 5);
+        SleepTimes st = times.get(locale.getCountry().toLowerCase());
+        if (st != null) {
+            return st.wakeUpTime;
         }
-        return new HourMinute(7, 20);
+        return times.get(DEFAULT_COUNTRY_CODE).wakeUpTime;
+    }
+
+    static class SleepTimes {
+        public HourMinute bedTime;
+        public HourMinute wakeUpTime;
+
+        public SleepTimes(HourMinute bedTime, HourMinute wakeUpTime) {
+            this.bedTime = bedTime;
+            this.wakeUpTime = wakeUpTime;
+        }
     }
 }
